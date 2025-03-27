48cf9791d68a92e13c484a714d7a4a7db8b87777
Pranjal Khare <pranjalkhare2004@gmail.com>
2025-03-27 11:05:42 +0530

    basics of getline

---
+++ Cp practice/1.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int number;
+    cin >> number;
+    // int a = sqrt(number);
+    // cout << "square root of " << number << " is " << a;
+
+    
+}
